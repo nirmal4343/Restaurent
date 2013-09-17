593480018397c3313b3160d1f0a9ee99d33813e6
NIrmal Thakur <nirmalthakur@unknown28cfe905142b.gateway.2wire.net>
2013-09-16 20:07:02 -0400

    Initial Commit

---
+++ Restaurent/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Restaurent
+//
+//  Created by NIrmal Thakur on 9/16/13.
+//  Copyright (c) 2013 NIrmal Thakur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Restaurent/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Restaurent
+//
+//  Created by NIrmal Thakur on 9/16/13.
+//  Copyright (c) 2013 NIrmal Thakur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
